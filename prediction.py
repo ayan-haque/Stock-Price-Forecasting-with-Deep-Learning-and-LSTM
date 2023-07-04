b4910863d6b79963bf02ae9cc40ac13720bf1183
Md Ayan Haque <110412621+ayan-haque@users.noreply.github.com>
2023-07-04 13:50:32 +0530

    prediction.py

--- prediction.py
+++ prediction.py
@@ -1,5 +1,3 @@
-# Part 3 - Making the predictions and visualising the results
-
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -8,11 +6,9 @@ class prediction():
     
     def pred(self, dataset_train, sc, regressor):
 
-        dataset = pd.read_csv('Tata_Global_Stock_Price_Test.csv')#importing the dataset
-        real_stock_price = dataset.iloc[:, 1:2].values#from the dataset the column is sliced to for getting the real stock price
-        
-        # in the block below trying to get the predicted stock prices
-        dataset_combined = pd.concat((dataset_train['Open'], dataset['Open']), axis = 0)#combining the columns to input previous 60 samples
+        dataset = pd.read_csv('Tata_Global_Stock_Price_Test.csv')
+        real_stock_price = dataset.iloc[:, 1:2].values
+        dataset_combined = pd.concat((dataset_train['Open'], dataset['Open']), axis = 0)
         inputs = dataset_combined[len(dataset_combined) - len(dataset) - 60:].values
         inputs = inputs.reshape(-1,1)
         inputs = sc.transform(inputs)
@@ -23,8 +19,6 @@ class prediction():
         X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
         predicted_price = regressor.predict(X_test)
         predicted_price = sc.inverse_transform(predicted_price)
-        
-        # Visualising the output between the real and predicted price
         plt.plot(real_stock_price, color = 'red', label = 'Original Stock Price')
         plt.plot(predicted_price, color = 'blue', label = 'Predicted Stock Price')
         plt.title('Tata Global Stock Price Prediction')
