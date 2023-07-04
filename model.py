22ace7793018f64c2ff71255d0d69b35a382d126
Md Ayan Haque <110412621+ayan-haque@users.noreply.github.com>
2023-07-04 13:48:38 +0530

    model.py

--- model.py
+++ model.py
@@ -1,43 +1,24 @@
-# Part 2 - Building the RNN
-# Importing the Keras libraries and packages
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 from keras.layers import Dropout
 
-
-
 class modeling:
     
     def model(self, X_train, y_train):
-        # Initialising the RNN
         
         regressor = Sequential()
-        # Adding the first LSTM layer and some Dropout regularisation
         regressor.add(LSTM(units = 50, return_sequences = True, input_shape = (X_train.shape[1], 1)))
         regressor.add(Dropout(0.2))
-        
-        # Adding a second LSTM layer and some Dropout regularisation
         regressor.add(LSTM(units = 50, return_sequences = True))
         regressor.add(Dropout(0.2))
-        
-        # Adding a third LSTM layer and some Dropout regularisation
         regressor.add(LSTM(units = 50, return_sequences = True))
         regressor.add(Dropout(0.2))
-        
-        # Adding a fourth LSTM layer and some Dropout regularisation
         regressor.add(LSTM(units = 50))
         regressor.add(Dropout(0.2))
-        
-        # Adding the output layer
         regressor.add(Dense(units = 1))
-        
-        # Compiling the RNN
         regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')
-        
-        # Fitting the RNN to the Training set
         regressor.fit(X_train, y_train, epochs = 100, batch_size = 32)
-        
         return regressor
         
     
